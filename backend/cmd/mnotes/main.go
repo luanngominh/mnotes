6d41e4de543f5f012eb2ee1b173de0b87d10b8f2
luanngominh <luannm@dwarvesv.com>
2019-01-02 03:20:25 -0500

    add connecto to db

--- backend/cmd/mnotes/main.go
+++ backend/cmd/mnotes/main.go
@@ -5,17 +5,15 @@ import (
 
 	"github.com/luanngominh/mnotes/backend/config"
 	_ "github.com/luanngominh/mnotes/backend/config"
+	"github.com/luanngominh/mnotes/backend/db"
 )
 
 func main() {
-	// _, deferDBFunc := db.New(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
-	// 	config.Cfg.DBHost, config.Cfg.DBPort,
-	// 	config.Cfg.DBUser, config.Cfg.DBName,
-	// 	config.Cfg.DBPassword, "disable"))
-
-	// defer deferDBFunc()
-	fmt.Printf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+	_, deferDBFunc := db.New(fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		config.Cfg.DBHost, config.Cfg.DBPort,
 		config.Cfg.DBUser, config.Cfg.DBName,
-		config.Cfg.DBPassword, "disable")
+		config.Cfg.DBPassword, "disable"))
+
+	defer deferDBFunc()
+
 }
