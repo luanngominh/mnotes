562d7845065d25d757dc000b7bb31f4fc60f2819
luanngominh <luannm@dwarvesv.com>
2019-01-02 02:58:21 -0500

    migration success

--- backend/migration/1_user.sql
+++ backend/migration/1_user.sql
@@ -1,19 +1,17 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
--- expires if user_id = Null, expires = 1 day
-
-CREATE TABLE "public"."note"(
+CREATE TABLE "public"."users" (
 	"id" uuid NOT NULL,
-	"user_id" uuid,
-	"title" text,
-	"body" text,
-	"tags" text[],
-	"expires" timestamptz,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"hashed_pass" text NOT NULL,
+	"status" smallint DEFAULT 0,
+	"access_code" text,
 	"created_at" timestamptz DEFAULT now(),
 	"deleted_at" timestamptz,
+	CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
-
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
--- backend/migration/2_note.sql
+++ backend/migration/2_note.sql
@@ -1,22 +1,21 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 
--- In status field, 0 is default. Because 0 is inactive, 1 is active and 2 is deactive.
-
-CREATE TABLE "public"."user" (
+CREATE TABLE "public"."notes"(
 	"id" uuid NOT NULL,
-	"name" text NOT NULL,
-	"email" text NOT NULL,
-	"hashed_pass" text NOT NULL,
-	"status" smallint DEFAULT 0,
-	"access_code" text,
+	"user_id" uuid,
+	"title" text,
+	"body" text,
+	"tags" text[],
+	"expires" timestamptz,
 	"created_at" timestamptz DEFAULT now(),
 	"deleted_at" timestamptz,
-	CONSTRANINT "user_pkey" PRIMARY KEY ("id")
+	CONSTRAINT "notes_pkey" PRIMARY KEY("id"),
+	CONSTRAINT "notes_users" FOREIGN kEY ("id") REFERENCES users("id")
 ) WITH (oids = false);
+
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-
 DROP SCHEMA public CASCADE;
 CREATE SCHEMA public;
