09ce80f2c4c66bb45d4cf84d59c103a85995b2a9
luanngominh <luannm@dwarvesv.com>
2019-01-28 01:39:02 -0500

    add tailwind css

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Login/>
+</template>
+
+<script>
+import Login from "./Login.vue"
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            loggedIn: false
+        }
+    },
+    components: {
+        Login,
+    }
+}
+</script>
+
