8d9d98bf0e7f347dcfca54c2a91e741871f96d84
LuanNgoMinh <Ngominhluanbox@gmail.com>
2019-02-26 12:54:30 +0700

    add login page content (#2)

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>
-        <div class="bg-indigo-darkest text-center py-4 lg:px-4">
-  <div class="p-2 bg-indigo-darker items-center text-indigo-lightest leading-none lg:rounded-full flex lg:inline-flex" role="alert">
-    <span class="flex rounded-full bg-indigo uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
-    <span class="font-semibold mr-2 text-left flex-auto">Get the coolest t-shirts from our brand new store</span>
-    <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
-  </div>
-</div>
-    </div>
+   <div>
+           <div>
+               <input type="input" v-model="username" placeholder="Username"/>
+               <input type="input" v-model="password" placeholder="password"/>
+               <input type="submit" value="Login" v-on:click="login">
+               <input type="submit" value="Register">
+           </div>
+           <div>Forgot password!</div>
+   </div>
 </template>
 
 <script>
 export default {
-    name: 'Login',
+    name: "Login",
+   
+    data() {
+        return {
+            username: "",
+            password: "",
+        }
+    },
+
+    methods: {
+        login() {
+            console.log("Login with", this.username, this.password)
+        },
+    }
 }
 </script>
